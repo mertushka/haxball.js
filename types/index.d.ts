06f9f58b356f513e43e4bf3fc75d721e75a4300d
jakjus <fazzebo@gmail.com>
2022-08-26 11:18:00 +0200

    add debug type def

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,7 @@ import "haxball-headless-browser";
 
 interface RoomConfigObjectM extends RoomConfigObject {
     proxy?: string
+    debug?: boolean
 }
 export declare type Headless = (RoomConfig: RoomConfigObjectM) => RoomObject;
 
