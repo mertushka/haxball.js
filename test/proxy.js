f0d193936779e4cd52630fe14a319fe084e7e2ee
mertushka <34413473+mertushka@users.noreply.github.com>
2025-01-06 17:36:45 +0300

    automated builds

--- test/proxy.js
+++ test/proxy.js
@@ -2,7 +2,7 @@ const HaxballJS = require("../src/build");
 
 describe("HBInit Tests (Proxy)", function () {
   it("should create room", function (done) {
-    this.timeout(20000);
+    this.timeout(30000);
     HaxballJS.then((HBInit) => {
       var room = HBInit({
         roomName: "Haxball.JS",
--- test/room.js
+++ test/room.js
@@ -2,7 +2,7 @@ const HaxballJS = require("../src/build");
 
 describe("HBInit Tests", function () {
   it("should create room", function (done) {
-    this.timeout(5000);
+    this.timeout(30000);
     HaxballJS.then((HBInit) => {
       var room = HBInit({
         roomName: "Haxball.JS",
