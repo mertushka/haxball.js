bd89ac100b090ec1cfb98ccf2828044b7244755f
mertushka <34413473+mertushka@users.noreply.github.com>
2025-01-07 16:07:43 +0300

    feat: ci/cd check for updates every week

--- test/room.js
+++ test/room.js
@@ -5,54 +5,56 @@ function requireUncached(module) {
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const TOKEN = "thr1.AAAAAGd82uXE_qA2Ongrnw.6AWO8_UYHKA"; // Make sure update here before testing
-const PROXY = "http://1.1.1.1:2222"; // (Optional) http://ip:port
-
 describe("HBInit Tests", function () {
   this.timeout(10000);
 
   beforeEach(async function () {
-    await delay(2000);
+    await delay(5000);
   });
 
-  it("should create room", function (done) {
-    this.timeout(30000);
-    requireUncached("../src/build").then((HBInit) => {
-      var room = HBInit({
-        roomName: "Haxball.JS",
-        maxPlayers: 16,
-        public: false,
-        noPlayer: true,
-        debug: true,
-        token: TOKEN,
-      });
-
-      room.onRoomLink = function () {
-        done();
-      };
-    });
-  });
-
-  // Optional proxy test
-  (process.env.TEST_PROXY ? it : it.skip)(
-    "should create room (proxy)",
-    function (done) {
+  if (!process.env.TEST_PROXY) {
+    it("should create room", function (done) {
       this.timeout(30000);
       requireUncached("../src/build").then((HBInit) => {
-        var room = HBInit({
+        let room = HBInit({
           roomName: "Haxball.JS",
           maxPlayers: 16,
           public: false,
           noPlayer: true,
           debug: true,
-          proxy: PROXY,
-          token: TOKEN,
+          token:
+            process.env.CI_HB_HEADLESS_TOKEN ||
+            process.env.TEST_HB_HEADLESS_TOKEN, // Fallback for local testing
         });
 
         room.onRoomLink = function () {
           done();
         };
       });
-    }
-  );
+    });
+  }
+
+  // Optional proxy test
+  if (process.env.TEST_PROXY) {
+    it("should create room (proxy)", function (done) {
+      this.timeout(30000);
+      requireUncached("../src/build").then((HBInit) => {
+        let room = HBInit({
+          roomName: "Haxball.JS",
+          maxPlayers: 16,
+          public: false,
+          noPlayer: true,
+          debug: true,
+          proxy: process.env.CI_HB_PROXY || process.env.TEST_HB_PROXY, // Fallback for local testing
+          token:
+            process.env.CI_HB_HEADLESS_TOKEN ||
+            process.env.TEST_HB_HEADLESS_TOKEN, // Fallback for local testing
+        });
+
+        room.onRoomLink = function () {
+          done();
+        };
+      });
+    });
+  }
 });
