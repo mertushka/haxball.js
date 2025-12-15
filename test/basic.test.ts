e2052cd994df100f0b0f30236070d91a4c3454a6
mertushka <mertushkaonline@gmail.com>
2025-12-15 02:55:59 +0200

    fix: tests

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -3,14 +3,14 @@ import HaxballJS from '../src'
 
 test('HBInit', (done) => {
 	HaxballJS({ debug: true }).then((HBInit) => {
-		expect(process.env.HEADLESS_TEST_TOKEN).toBeDefined()
+		expect(process.env.CI_HB_HEADLESS_TOKEN).toBeDefined()
 
 		const room = HBInit({
 			roomName: 'Haxball.JS',
 			maxPlayers: 16,
 			public: false,
 			noPlayer: true,
-			token: process.env.HEADLESS_TEST_TOKEN,
+			token: process.env.CI_HB_HEADLESS_TOKEN,
 		})
 
 		room.setDefaultStadium('Big')
